7cee2bc12772f28ab4e0cb0704b40e1da152172f
Yu Qian <jinggeqianyu1991@gmail.com>
2016-05-29 16:38:00 -0400

    add flesh

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+var path = require('path');
+var webpack = require('webpack');
+var config = require('./webpack.config');
 var express = require('express');
 var stormpath = require('express-stormpath');
  
- var app = express();
+var app = express();
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
   
 app.use(stormpath.init(app, {
   web: {
@@ -16,4 +26,13 @@ app.on('stormpath.ready', function () {
     }
     console.log('Listening at http://localhost:3000');
   });
+});
+
+stormpath.init(app, { website: true });
+app.get('/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/css/bootstrap.min.css'));
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
