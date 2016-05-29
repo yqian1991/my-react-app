26c83e74382e23d26a82c4c7ad87b1d77bfebe78
Yu Qian <jinggeqianyu1991@gmail.com>
2016-05-29 16:08:37 -0400

    update

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ var stormpath = require('express-stormpath');
  
  var app = express();
   
-  app.use(stormpath.init(app, {
-    web: {
-        produces: ['application/json']
-          }
-          }));
+app.use(stormpath.init(app, {
+  web: {
+    produces: ['application/json']
+  }
+}));
            
-  app.on('stormpath.ready', function () {
-             app.listen(3000, 'localhost', function (err) {
-                 if (err) {
-                       return console.error(err);
-                  }
-                  console.log('Listening at http://localhost:3000');
-             });
- });
+app.on('stormpath.ready', function () {
+  app.listen(3000, 'localhost', function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Listening at http://localhost:3000');
+  });
+});
